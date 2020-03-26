cdfa29c2896b33ae597a1ad530f1a58b11fbbdb5
Jonas <50097340+JKutscha@users.noreply.github.com>
2020-03-26 09:45:20 +0100

    updated angular and prepared for start

--- AngularForWebApp/src/app/app.module.ts
+++ AngularForWebApp/src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+
+let routes: Routes;
+routes = [
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
