8e1f631235a5b06fe7f4ed4e1ea13463c0811ecc
Jonas <50097340+JKutscha@users.noreply.github.com>
2020-03-26 11:02:25 +0100

    first nav with bootstrap

--- AngularForWebApp/src/app/app.component.ts
+++ AngularForWebApp/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+		selector: 'app-root',
+		templateUrl: './app.component.html',
+		styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Web App Software Engineering';
-  projectName = 'PROMAS';
+		projectName = 'PROMAS';
+		constructor() {
+			this.projectName = 'test';
+		}
 }
