0509f555c31d632802aaf4f9a8942a0034735530
Matthias Brehmer <m@brehmer-adf.de>
2020-04-01 19:36:00 +0200

    Revert "Revert "Create C# classes from database schema""
    This reverts commit d1936f5c6a749fd8dfb315d5fd6c2601a0ee490e.

---
+++ API/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API
+{
+    public partial class Task
+    {
+        public Task()
+        {
+            TaskAssignment = new HashSet<TaskAssignment>();
+        }
+
+        public long TaskId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long ProjectId { get; set; }
+
+        public virtual Project Project { get; set; }
+        public virtual ICollection<TaskAssignment> TaskAssignment { get; set; }
+    }
+}
---
+++ API/TaskAssignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API
+{
+    public partial class TaskAssignment
+    {
+        public string Username { get; set; }
+        public long TaskId { get; set; }
+
+        public virtual Task Task { get; set; }
+        public virtual User UsernameNavigation { get; set; }
+    }
+}
