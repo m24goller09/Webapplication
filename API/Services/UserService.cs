ef349cf13edf54c41eaf0960c27c7a11e21dd1ee
christoph <inf18174@lehre.dhbw-stuttgart.de>
2020-04-08 23:17:24 +0200

    Initial commit for user service class.

---
+++ API/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models.Services;
+using API.Domain.Models;
+using API.Domain.Repository;
+
+
+namespace API.Services
+{
+    public class UserService : AbstractStandardService <User>
+    {
+        UserService(IStandardRepository<User> userRepository, IUnitOfWork unitOfWork) : base (userRepository, unitOfWork)
+        {
+
+        }
+    }
+}
