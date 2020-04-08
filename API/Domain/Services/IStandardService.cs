ffa03d9ce3fbb579ae04cdf4a9d0bd276ddd7d1c
christoph <inf18174@lehre.dhbw-stuttgart.de>
2020-04-08 22:43:42 +0200

    Added standard interface for the service classes.

---
+++ API/Domain/Services/IStandardService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Domain.Services
+{
+    public interface IStandardService <Model>
+    {
+        Task<IEnumerable<Model>> ListAsync();
+        Task AddAsync(Model model);
+        Task<Model> FindByIdAsync(int id);
+        void Update(Model model);
+    }
+}
+
