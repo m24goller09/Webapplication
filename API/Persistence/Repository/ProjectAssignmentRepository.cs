7360a7eb94e5f00a47cc2f93052810b24e30eb4f
m24goller09 <Mario.Goller@web.de>
2020-04-09 11:14:43 +0200

    implementet missing repository classes

---
+++ API/Persistence/Repository/ProjectAssignmentRepository.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Persistence.Context;
+using API.Domain.Repositories;
+using API.Domain.Models;
+
+namespace API.Persistence.Repository
+{
+    public class ProjectAssignmentRepository : IStandardRepository<ProjectAssignment>
+    {
+        private readonly dbContext dbContext;
+        ProjectAssignmentRepository(dbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task AddAsync(ProjectAssignment projAssign)
+        {
+            await dbContext.ProjectAssignment.AddAsync(projAssign);
+        }
+
+        public async Task<ProjectAssignment> FindByIdAsync(int id)
+        {
+            // Check if project assignment is found will be made in the service class.
+            return await dbContext.ProjectAssignment.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<ProjectAssignment>> ListAsync()
+        {
+            return await dbContext.ProjectAssignment
+                .Include(d => d.ProjectNavigation) //JOIN
+                .Include(d => d.UsernameNavigation) //JOIN
+                .Include(d => d.Project).ThenInclude(d => d.Subtask) // Multiple JOINS
+                .ToListAsync();
+        }
+
+        public void Update(ProjectAssignment projAssign)
+        {
+            dbContext.ProjectAssignment.Update(projAssign);
+        }
+
+        public void Remove(ProjectAssignment projAssign)
+        {
+            dbContext.ProjectAssignment.Remove(projAssign);
+        }
+    }
+}
---
+++ API/Persistence/Repository/SubtaskAssignmentRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Persistence.Context;
+using API.Domain.Repositories;
+using API.Domain.Models;
+
+namespace API.Persistence.Repository
+{
+    public class SubtaskAssignmentRepository : IStandardRepository<SubtaskAssignment>
+    {
+        private readonly dbContext dbContext;
+        SubtaskAssignmentRepository(dbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task AddAsync(SubtaskAssignment stAssign)
+        {
+            await dbContext.SubtaskAssignment.AddAsync(stAssign);
+        }
+
+        public async Task<SubtaskAssignment> FindByIdAsync(int id)
+        {
+            // Check if subtask assignment is found will be made in the service class.
+            return await dbContext.SubtaskAssignment.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<SubtaskAssignment>> ListAsync()
+        {
+            return await dbContext.SubtaskAssignment
+                .Include(d => d.UsernameNavigation) //JOIN
+                .Include(d => d.Subtask) // JOIN
+                .ToListAsync();
+        }
+
+        public void Update(SubtaskAssignment stAssign)
+        {
+            dbContext.SubtaskAssignment.Update(stAssign);
+        }
+
+        public void Remove(SubtaskAssignment stAssign)
+        {
+            dbContext.SubtaskAssignment.Remove(stAssign);
+        }
+    }
+}
