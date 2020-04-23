65f3f2d2591f31f66649b53e3698850d565b4bf0
christoph <inf18174@lehre.dhbw-stuttgart.de>
2020-04-23 20:27:07 +0200

    Added data annotations.

--- API/DTOs/UserDTO.cs
+++ API/DTOs/UserDTO.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class UserDTO
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Firstname { get; set; }
+        [Required]
         public string Lastname { get; set; }
     }
 }
