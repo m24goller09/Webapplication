eed664ea425b1bb13584418651b1d0ae3311cd07
christoph <inf18174@lehre.dhbw-stuttgart.de>
2020-04-23 17:25:41 +0200

    Added DTOtoModel-mapping for post and put controller methods.

---
+++ API/Mapping/DTOtoModel.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+using API.Domain.Models;
+using API.DTOs;
+
+
+namespace API.Mapping
+{
+    public class DTOtoModel : Profile
+    {
+        public DTOtoModel()
+        {
+            CreateMap<UserDTO, User>();
+        }
+    }
+}
