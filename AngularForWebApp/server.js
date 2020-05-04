d7901ed8ae617ee7f686f5fb85defaa0b76c732a
Jonas <50097340+JKutscha@users.noreply.github.com>
2020-05-04 13:15:48 +0200

    added procfile with default web for hekoru

--- AngularForWebApp/server.js
+++ AngularForWebApp/server.js
@@ -1,16 +1,14 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve static files
 app.use(express.static(__dirname + '/dist/WebAppSoftwareEngineering'));
 
-app.get('/*', function(req,res) {
-
-	res.sendFile(path.join(__dirname+'/dist/WebAppSoftwareEngineering/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/WebAppSoftwareEngineering/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
