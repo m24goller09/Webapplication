db56c5b5738831a2fa4ef3aa8d2aac26c8ef06ad
christoph <inf18174@lehre.dhbw-stuttgart.de>
2020-06-01 20:48:42 +0200

    Added forgotten file...

---
+++ API/API/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Exceptions
+{
+    public class UnauthorizedException : CustomException
+    {
+        public UnauthorizedException(string detail, string instance)
+        {
+            problemDetails = new ProblemDetails
+            {
+                Status = 401,
+                Type = "https://httpstatuses.com/401",
+                Title = "unauthorized",
+                Detail = detail,
+                Instance = instance
+            };
+        }
+
+        public override IActionResult GetActionResult()
+        {
+            return new BadRequestObjectResult(problemDetails);
+        }
+    }
+}
